73c374364745d957d60973d2e7c33cce3b12915b
Bruno Alla <alla.brunoo@gmail.com>
2025-04-11 19:16:55 +0100

    feat: add support for Django 4.0

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
     ],
     tests_require=['tox'],
     cmdclass={'test': Tox},
