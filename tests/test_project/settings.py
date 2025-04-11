fd15a252979eb7085786b17956684abd1be219fe
Bruno Alla <alla.brunoo@gmail.com>
2025-04-11 16:01:57 +0100

    ci: align setting for POSTGRES_PASSWORD

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'migrate_sql_test_db',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': 5432,
     }
