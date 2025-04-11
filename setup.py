e557917ceb37a91d260a367b56408dd62df9f5c4
Bruno Alla <alla.brunoo@gmail.com>
2025-04-11 18:57:01 +0100

    feat: add support for Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -64,6 +64,8 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
