5269735a2bdb1ec857222b439e4f453f3c8c4c7a
Bruno Alla <alla.brunoo@gmail.com>
2025-04-14 09:14:12 +0100

    chore: more Ruff fixes

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
+    """A book model."""
+
     name = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     rating = models.IntegerField(null=True, blank=True)
     published = models.BooleanField(default=True)
 
-    def __unicode__(self):
+    def __str__(self):
+        """Visual representation of instances."""
         return f"Book [{self.name}]"
