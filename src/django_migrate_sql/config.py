eb58cd74cf737389aa289f65dde2e86ee61fc729
𝕂 <51281148+K-dash@users.noreply.github.com>
2025-10-09 04:19:58 +0900

    feat: apply `textwrap.dedent()` to SQL strings automatically (#129)

--- src/django_migrate_sql/config.py
+++ src/django_migrate_sql/config.py
@@ -1,3 +1,6 @@
+import textwrap
+
+
 class SQLItem:
     """
     Represents any SQL entity (unit), for example function, type, index or trigger.
@@ -21,7 +24,31 @@ class SQLItem:
 
         """
         self.name = name
-        self.sql = sql
-        self.reverse_sql = reverse_sql
+        self.sql = self._process_sql(sql)
+        self.reverse_sql = self._process_sql(reverse_sql)
         self.dependencies = dependencies or []
         self.replace = replace
+
+    def _process_sql(self, sql):
+        """
+        Process SQL by applying textwrap.dedent() to strings.
+
+        Args:
+            sql (str/tuple/list): SQL string or tuple/list of (sql, params).
+
+        Returns:
+            Processed SQL in the same format as input.
+
+        """
+        if isinstance(sql, str):
+            return textwrap.dedent(sql)
+        if isinstance(sql, (tuple, list)):
+            return [
+                (textwrap.dedent(item[0]), item[1])
+                if isinstance(item, (tuple, list))
+                else textwrap.dedent(item)
+                if isinstance(item, str)
+                else item
+                for item in sql
+            ]
+        return sql
